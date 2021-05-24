52971a1368e1fed6acef476949e3668eeb3f6232
unknown <ken.matheis@gmail.com>
2021-05-24 15:24:10 -0400

    first commit

---
+++ src/main/java/com/kmatheis/vet/VetApiDemo.java
@@ -0,0 +1,13 @@
+package com.kmatheis.vet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetApiDemo {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( VetApiDemo.class, args );
+	}
+
+}
