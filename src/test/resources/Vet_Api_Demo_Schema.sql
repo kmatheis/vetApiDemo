ada1fdf9a5436d7e73982decb1987aa310e68293
Ken Matheis <ken.matheis@gmail.com>
2021-07-10 20:17:02 -0400

    db changes

--- src/test/resources/Vet_Api_Demo_Schema.sql
+++ src/test/resources/Vet_Api_Demo_Schema.sql
@@ -1,3 +1,8 @@
+drop table if exists reservations;
+drop table if exists owners;
+drop table if exists animals;
+drop table if exists profiles;
+drop table if exists rooms;
 drop table if exists users;
 drop table if exists privs;
 drop table if exists roles;
@@ -6,14 +11,14 @@ drop table if exists server_keys;
 create table if not exists server_keys (
     id int unsigned not null auto_increment,
     server_key varchar(90),
-    primary key( id )
+    primary key ( id )
 );
 
 -- e.g., ADMIN, TECH, RECEPTIONIST
 create table if not exists roles (
     id int unsigned not null auto_increment,
     rolename varchar(20),
-    primary key( id )
+    primary key ( id )
 );
 
 -- e.g., add users, edit animals, read profiles, del reservations, all users
@@ -21,8 +26,8 @@ create table if not exists privs (
     id int unsigned not null auto_increment,
     description varchar(20) not null,
     role_id int unsigned not null,
-    primary key( id ),
-    foreign key( role_id ) references roles( id )
+    primary key ( id ),
+    foreign key ( role_id ) references roles ( id )
 );
 
 create table if not exists users (
@@ -30,7 +35,56 @@ create table if not exists users (
     username varchar(40) not null,
     hash varchar(255) not null,
     role_id int unsigned not null,
-    primary key( id ),
-    unique key( username ),
-    foreign key( role_id ) references roles( id )
+    primary key ( id ),
+    unique key ( username ),
+    foreign key ( role_id ) references roles ( id )
+);
+
+create table if not exists rooms (
+    pk int unsigned not null auto_increment,
+    id int unsigned not null,
+    name varchar(40) not null,
+    maxcap int unsigned not null,
+    cost decimal( 5, 2 ) not null,
+    primary key ( pk )
+);
+
+create table if not exists profiles (
+    pk int unsigned not null auto_increment,
+    id int unsigned not null,
+    name varchar(40) not null,
+    primary key ( pk )
+);
+
+create table if not exists animals (
+    pk int unsigned not null auto_increment,
+    id int unsigned not null,
+    name varchar(40) not null,
+    species enum( 'OTHER', 'CAT', 'DOG', 'SUGAR_GLIDER', 'TURTLE', 'TORTOISE', 'IGUANA', 'BIRD', 'RODENT' ),
+    profile_fk int unsigned not null,
+    primary key ( pk ),
+    foreign key ( profile_fk ) references profiles( pk ) on delete cascade
+);
+
+create table if not exists owners (
+    pk int unsigned not null auto_increment,
+    id int unsigned not null,
+    name varchar(40) not null,
+    phone varchar(40),
+    profile_fk int unsigned not null,
+    primary key ( pk ),
+    foreign key ( profile_fk ) references profiles( pk ) on delete cascade
+);
+
+-- Implicit many-to-many table: res ids are dynamically generated instead of statically stored.
+-- This is just to demo how to manage many-to-many without an explicit reservation entity.
+create table if not exists reservations (
+    animal_fk int unsigned not null,
+    room_fk int unsigned not null,
+    fromdate date not null,
+    todate date not null,
+    paid tinyint not null default 0,
+    foreign key ( animal_fk ) references animals ( pk ) on delete cascade,
+    foreign key ( room_fk ) references rooms( pk ) on delete cascade,
+    unique key ( animal_fk, room_fk, fromdate )
 );
